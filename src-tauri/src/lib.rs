f6d966ade2c10fd1bb20eca542cb0057ec36f8b3
earljohn004 <earl.abaquita@gmail.com>
2025-03-16 20:40:59 +0800

    add system tray

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,9 @@
 use std::time::Duration;
 
 use http_server::handler::start_server;
+use tauri::menu::{ Menu, MenuItem };
 use tauri::{ AppHandle, Emitter };
+use tauri::tray::TrayIconBuilder;
 use tokio::sync::mpsc;
 use tokio::time::sleep;
 
@@ -19,6 +21,46 @@ pub fn run() {
         .setup(|app| {
             let app_handle = app.handle().clone();
 
+            let show_main_i = MenuItem::with_id(
+                app,
+                "show_main",
+                "Show Main Window",
+                true,
+                None::<&str>
+            )?;
+            let show_small_i = MenuItem::with_id(
+                app,
+                "show_small",
+                "Show Small Window",
+                true,
+                None::<&str>
+            )?;
+            let quit_i = MenuItem::with_id(app, "quit", "Quit", true, None::<&str>)?;
+
+            let menu = Menu::with_items(app, &[&quit_i, &show_small_i, &show_main_i])?;
+            let _ = TrayIconBuilder::new()
+                .on_menu_event(|app_handle, event| {
+                    match event.id.as_ref() {
+                        "quit" => {
+                            println!("quit menu item was clicked");
+                            app_handle.exit(0);
+                        }
+                        "show_small" => {
+                            println!("show small was clicked");
+                        }
+                        "show_main" => {
+                            println!("show main was clicked");
+                        }
+                        _ => {
+                            println!("menu item {:?} not handled", event.id);
+                        }
+                    }
+                })
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .show_menu_on_left_click(true)
+                .build(app);
+
             let (tx, mut rx): (
                 mpsc::Sender<(u64, AppHandle)>,
                 mpsc::Receiver<(u64, AppHandle)>,
@@ -34,7 +76,6 @@ pub fn run() {
 
                     let _ = app_handle.emit("addtime_handler", total_time);
                     let _ = app_handle.emit("timer_update", remaining_time);
-
 
                     while remaining_time > 0 {
                         sleep(Duration::from_secs(1)).await;
