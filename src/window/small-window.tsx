4b457f73dba074ea33edee326f9cca2f6cdd3260
Earl John Abaquita <earl.abaquita@gmail.com>
2025-03-23 00:58:41 +0800

    Implement react router (#7)
    - Implement react router
    - Implement changing windows from main to small when coin is inserted

---
+++ src/window/small-window.tsx
@@ -0,0 +1,12 @@
+import { useSettingStore } from "../SettingStore";
+
+export const SmallWindow = () => {
+  const coin = useSettingStore((state) => state.coin);
+  const remainingTime = useSettingStore((state) => state.remainingTime);
+  return (
+    <>
+      <h2>Inserted PHP {coin}</h2>
+      <h3>Remaining Time: {remainingTime} seconds</h3>
+    </>
+  );
+};
